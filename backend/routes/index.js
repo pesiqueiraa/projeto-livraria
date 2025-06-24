4e750f5a52432ba2f91dc2e207d3413b08d901b9
Pedro Siqueira <pedrosiqueira104@gmail.com>
2025-06-23 22:39:16 -0300

    feat: add user and admin routes config

---
+++ backend/routes/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+// === ROTAS DO USUÁRIO === //
+// === ROTAS DO ADMIN === //
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+const express = require('express'); // O Express é um framework para Node.js que facilita a criação de servidores web
+const path = require('path'); // O path é usado para manipular caminhos de arquivos e diretórios
+const cors = require('cors'); ; // O Cors serve para permitir requisições de outros domínios
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +8,9 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const routes = require('./routes/index.js'); // Importa as rotas definidas no arquivo routes.js
+app.use('/api', routes); // Define o prefixo '/api' para as rotas
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
