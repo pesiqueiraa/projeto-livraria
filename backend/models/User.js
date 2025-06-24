5692b4bdb494db6b33deb79f6528ec39ddbc6995
Pedro <pedrosiqueirasouza6@gmail.com>
2025-06-24 08:54:56 -0300

    feat: implement User model with methods for creating and finding users

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,66 @@
 
+const db = require('../config/database');
+
+class User {
+
+    constructor (id, name, email, password){
+        this.id = id,
+        this.name = name,
+        this.email = email,
+        this.password = password
+    }
+
+    // create a new user 
+    static async create(name, email, password) {
+
+        // Validate unique email
+        const existingUser = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+        if (existingUser.rows.length > 0) {
+            throw new Error('Email already exists');
+        }
+
+        try {
+            const result = await db.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, password])
+            const user = result.rows[0];
+            return new User(user.id, user.name, user.email, user.password);
+
+        } catch (error){
+            console.log("Erro ao criar usuário: ", error);
+            throw error;
+        }
+    }
+
+    // find user by email
+    static async findByEmail(email) {
+        try {
+            const result = await db.query('SELECT * FROM users WHERE email = $1', [email]);
+            if (result.rows.length > 0) {
+                const user = result.rows[0];
+                return new User(user.id, user.name, user.email, user.password);
+            } else {
+                return null; // User not found
+            }
+        } catch (error) {
+            console.log("Erro ao buscar usuário por email: ", error);
+            throw error;
+        }
+    }
+
+    // find user by id
+    static async findById(id) {
+        try {
+            const result = await db.query('SELECT * FROM users WHERE id = $1', [id])
+            if (result.rows.length > 0){
+                const user = result.rows[0];
+                return new User(user.id, user.name, user.email, user.password);
+            } else {
+                return null; // User not found
+            }
+        } catch (error) {  
+            console.log("Erro ao buscar usuário por ID: ", error);
+            throw error;
+        } 
+    }
+    
+
+}
