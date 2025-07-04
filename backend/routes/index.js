77d3b699c41f79f288217998e2bcfb5689a078d6
Pedro Siqueira <pedrosiqueira104@gmail.com>
2025-07-03 22:16:09 -0300

    feat: add user creation route to index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+const UserController = require('../controllers/UserController');
 // === ROTAS DO USUÁRIO === //
+router.post('/users', UserController.createUser); // Criar usuário
 // === ROTAS DO ADMIN === //
 module.exports = router
