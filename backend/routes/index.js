29ba9bc92cd14673d213a61e2264a40e72fed423
Pedro Siqueira <pedrosiqueira104@gmail.com>
2025-07-03 23:59:39 -0300

    feat: add book routes for CRUD operations in index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,5 +4,11 @@ const UserController = require('../controllers/UserController');
 const BookController = require('../controllers/BookController');
 // === ROTAS DO USUÁRIO === //
 router.post('/users', UserController.createUser); // Criar usuário
-// === ROTAS DO ADMIN === //
+// === ROTAS DO BOOK === //
+router.get('/books', BookController.getAllBooks); // Listar todos os livros
+router.get('/books/:id', BookController.getBookById); // Obter livro por ID
+router.post('/books', BookController.createBook); // Criar livro
+router.put('/books/:id', BookController.updateBook); // Atualizar livro por ID
+router.delete('/books/:id', BookController.deleteBook); // Deletar livro por ID
+
 module.exports = router
