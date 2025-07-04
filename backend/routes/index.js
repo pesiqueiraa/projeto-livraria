4a90c59469a322e493fbf563fa3b3068bc2db003
Pedro Siqueira <pedrosiqueira104@gmail.com>
2025-07-03 23:01:55 -0300

    feat: import BookController in index.js for future route implementation

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
+const BookController = require('../controllers/BookController');
 // === ROTAS DO USUÁRIO === //
 router.post('/users', UserController.createUser); // Criar usuário
 // === ROTAS DO ADMIN === //
