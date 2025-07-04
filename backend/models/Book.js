c2d2a01c5356b352dbecd769db4f20bee1b30094
Pedro Siqueira <pedrosiqueira104@gmail.com>
2025-07-03 23:59:50 -0300

    feat: refactor Book model to include genre_id, read_date, rating, and review in constructor and methods

--- backend/models/Book.js
+++ backend/models/Book.js
@@ -2,26 +2,28 @@ const db = require('../config/database');
 
 class Book {
 
-    constructor(id, title, author, genre, publishedYear) {
+    constructor(id, title, author, genre_id, read_date, rating, review) {
         this.id = id;
         this.title = title;
         this.author = author;
-        this.genre = genre;
-        this.publishedYear = publishedYear;
+        this.genre_id = genre_id;
+        this.read_date = read_date;
+        this.rating = rating;
+        this.review = review;
     }
     // Metodo para criar um novo livro
-    static async create(title, author, genre, publishedYear) {
-        const query = 'INSERT INTO books (title, author, genre, published_year) VALUES ($1, $2, $3, $4) RETURNING *';
-        const values = [title, author, genre, publishedYear];
+    static async create(title, author, genre_id, read_date, rating, review) {
+        const query = 'INSERT INTO books (title, author, genre_id, read_date, rating, review) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *';
+        const values = [title, author, genre_id, read_date, rating, review];
         const result = await db.query(query, values);
-        return new Book(result.rows[0].id, result.rows[0].title, result.rows[0].author, result.rows[0].genre, result.rows[0].published_year);
+        return new Book(result.rows[0].id, result.rows[0].title, result.rows[0].author, result.rows[0].genre_id, result.rows[0].read_date, result.rows[0].rating, result.rows[0].review);
     }
 
     // Metodo para buscar todos os livros
     static async getAll() {
         const query = 'SELECT * FROM books';
         const result = await db.query(query);
-        return result.rows.map(row => new Book(row.id, row.title, row.author, row.genre, row.published_year));
+        return result.rows.map(row => new Book(row.id, row.title, row.author, row.genre_id, row.read_date, row.rating, row.review));
     }
 
     // Metodo para buscar um livro por ID
@@ -33,18 +35,18 @@ class Book {
             throw new Error('Book not found');
         }
         const row = result.rows[0];
-        return new Book(row.id, row.title, row.author, row.genre, row.published_year);
+        return new Book(row.id, row.title, row.author, row.genre_id, row.read_date, row.rating, row.review);
     }
     // Metodo para atualizar um livro por ID
-    static async updateById(id, title, author, genre, publishedYear) {
-        const query = 'UPDATE books SET title = $1, author = $2, genre = $3, published_year = $4 WHERE id = $5 RETURNING *';
-        const values = [title, author, genre, publishedYear, id];
+    static async updateById(id, title, author, genre_id, read_date, rating, review) {
+        const query = 'UPDATE books SET title = $1, author = $2, genre_id = $3, read_date = $4, rating = $5, review = $6 WHERE id = $7 RETURNING *';
+        const values = [title, author, genre_id, read_date, rating, review, id];
         const result = await db.query(query, values);
         if (result.rows.length === 0) {
             throw new Error('Book not found');
         }
         const row = result.rows[0];
-        return new Book(row.id, row.title, row.author, row.genre, row.published_year);
+        return new Book(row.id, row.title, row.author, row.genre_id, row.read_date, row.rating, row.review);
     }
 
     // Metodo para encontrar por nome do livro
@@ -55,7 +57,7 @@ class Book {
         if (result.rows.length === 0) {
             throw new Error('Book not found');
         }
-        return result.rows.map(row => new Book(row.id, row.title, row.author, row.genre, row.published_year));
+        return result.rows.map(row => new Book(row.id, row.title, row.author, row.genre_id, row.read_date, row.rating, row.review));
     }
 
     // Metodo para deletar um livro por ID
@@ -66,7 +68,7 @@ class Book {
         if (result.rows.length === 0) {
             throw new Error('Book not found');
         }
-        return new Book(result.rows[0].id, result.rows[0].title, result.rows[0].author, result.rows[0].genre, result.rows[0].published_year);
+        return new Book(result.rows[0].id, result.rows[0].title, result.rows[0].author, result.rows[0].genre_id, result.rows[0].read_date, result.rows[0].rating, result.rows[0].review);
     }
 }
 
